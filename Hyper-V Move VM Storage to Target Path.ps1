034c25dd074b5f6c0661bf94a530f4cf0ead1afd
hypercube33 <hypercube33@hotmail.com>
2022-04-12 21:58:40 -0500

    Add files via upload

---
+++ Hyper-V Move VM Storage to Target Path.ps1
@@ -0,0 +1,18 @@
+﻿<#
+Brian Thorp
+2022-04-12
+
+Moves Hyper-V VMs on localhost to another drive.
+
+Keeps the folders clean
+
+#>
+$Destination = "E:\VM\"
+
+$VMs = Get-VM
+
+foreach($vm in $VMs)
+{
+    $name = $vm.name
+    Move-VMStorage -VMName "$name" -DestinationStoragePath "$Destination\$name" -asjob
+}
